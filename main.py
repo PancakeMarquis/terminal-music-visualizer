290e23bba23ef6cec970e5645e3ffd037f2d10e1
PancakeMarquis <diego18902@gmail.com>
2026-06-23 17:00:13 -0600

    Initlizacion of project with basic prototyping

---
+++ main.py
@@ -0,0 +1,77 @@
+import random
+import os
+from time import sleep
+import wave
+import numpy as np
+
+
+def main():
+    is_running: bool = True
+    
+   
+    file = get_audio_file()
+    print(f"Playing {file}")
+
+    audio = load_audio(file)
+    try:
+        
+        while is_running:
+            chunk = get_next_chunk(audio)
+            bars = analyze_chunk(chunk)
+            run_visualizer(bars)
+
+            if audio.getnframes() == audio.tell():
+                break
+            
+
+    except KeyboardInterrupt:
+        print(f"Stopped playing {file}")
+    
+def get_audio_file() -> str:
+    file: str = input("Choose an audio file: ")
+    return file
+
+def load_audio(file: str):
+    audio = wave.open(file)
+    return audio
+    
+
+def get_next_chunk(audio):
+    frames = audio.readframes(1024)
+
+    if len(frames) == 0:
+        return None
+    
+    samples = np.frombuffer(frames, dtype=np.int16)
+
+    return samples
+
+
+def analyze_chunk(chunk):
+    if chunk is None:
+        return None
+    amplitude = np.mean(np.abs(chunk))
+
+    bars = int(amplitude / 32767 * 20)
+
+    return bars
+
+def play_audio(file_data):
+    pass
+
+def run_visualizer(bars):
+    os.system("clear")
+    draw_bars(bars)
+    sleep(.023)
+    
+    
+
+def draw_bars(bars: int):
+    bar = ""
+    for num in range(bars):
+        bar += "#"
+    print(bar)
+
+
+if __name__ == "__main__":
+    main()
